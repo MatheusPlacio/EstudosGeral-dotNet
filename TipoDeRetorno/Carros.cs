396f0ef83a661f1edf7e3abafca788c06333e4d3
narba <narba@DESKTOP-KRRRREC>
2022-12-01 20:36:00 -0300

    Alteracoes

---
+++ TipoDeRetorno/Carros.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoDeRetorno
+{
+    public class Carros
+    {
+        public Pessoa PessoaId { get; set; }
+        public Pessoa Pessoa { get; set; }
+        public int CarrosId { get; set; }
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public string Cor { get; set; }
+    }
+}
--- TipoDeRetorno/Pessoa.cs
+++ TipoDeRetorno/Pessoa.cs
@@ -8,5 +8,13 @@ namespace TipoDeRetorno
 {
     public class Pessoa
     {
+        public int PessoaId { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string CorDePele { get; set; }
+        public string Magro { get; set; }
+        public string Obeso { get; set; }
+        public decimal altura { get; set; }
+        public List<Carros> Carros { get; set; }
     }
 }
